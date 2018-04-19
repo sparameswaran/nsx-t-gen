7e752750c9a0d07f116a4f64f200418f63c1badb
Sabha Parameswaran <sabhap@pivotal.io>
2018-04-19 19:12:01 -0400

    cleanup

--- functions/copy_ovas.sh
+++ functions/copy_ovas.sh
@@ -6,40 +6,29 @@ export OVA_ISO_PATH='/root/ISOs/CHGA'
 function install_ovftool {
 
 	# Install provided ovftool
-	if [ -e ovftool ]; then
-	  cd ovftool
+	if [ ! -e "/usr/bin/ovftool" ]; then
+	  pushd $ROOT_DIR/ovftool
 	  ovftool_bundle=$(ls *)
 	  chmod +x $ovftool_bundle
-	  ./$ovftool_bundle --eulas-agreed
-	  cd ..
+	  ./${ovftool_bundle} --eulas-agreed
+	  popd
 	  echo "Done installing ovftool"
 	else
-		echo "ovftool already installed!!"
+	  echo "ovftool already installed!!"
 	fi
 	echo ""	
 }
 
 function copy_ovas_to_OVA_ISO_PATH {
 
-	# Install provided ovftool
-	if [ -e ovftool ]; then
-	  cd ovftool
-	  ovftool_bundle=$(ls *)
-	  chmod +x $ovftool_bundle
-	  ./$ovftool_bundle --eulas-agreed
-	  cd ..
-	fi
-	echo "Done installing ovftool"
-	echo ""
-
-	NSX_T_MANAGER_OVA=$(ls nsx-mgr-ova)
-	NSX_T_CONTROLLER_OVA=$(ls nsx-ctrl-ova)
-	NSX_T_EDGE_OVA=$(ls nsx-edge-ova)
+	NSX_T_MANAGER_OVA=$(ls $ROOT_DIR/nsx-mgr-ova)
+	NSX_T_CONTROLLER_OVA=$(ls $ROOT_DIR/nsx-ctrl-ova)
+	NSX_T_EDGE_OVA=$(ls $ROOT_DIR/nsx-edge-ova)
 
 	mkdir -p $OVA_ISO_PATH
-	cp nsx-mgr-ova/$NSX_T_MANAGER_OVA \
-	   nsx-ctrl-ova/$NSX_T_CONTROLLER_OVA \
-	   nsx-edge-ova/$NSX_T_EDGE_OVA \
+	cp $ROOT_DIR/nsx-mgr-ova/$NSX_T_MANAGER_OVA \
+	   $ROOT_DIR/nsx-ctrl-ova/$NSX_T_CONTROLLER_OVA \
+	   $ROOT_DIR/nsx-edge-ova/$NSX_T_EDGE_OVA \
 	   $OVA_ISO_PATH
 
 	echo "Done copying ova images into $OVA_ISO_PATH"
