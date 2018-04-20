5ec832ee3dd7497cbb3c7bf55a8eea7b15cfd1cf
Sabha Parameswaran <sabhap@pivotal.io>
2018-04-19 23:34:25 -0400

    cleanup

---
+++ tasks/add-nsx-t-routers/task.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e
+
+export ROOT_DIR=`pwd`
+
+export TASKS_DIR=$(dirname $BASH_SOURCE)
+export PIPELINE_DIR=$(cd $TASKS_DIR/../../ && pwd)
+export FUNCTIONS_DIR=$(cd $PIPELINE_DIR/functions && pwd)
+
+export OVA_ISO_PATH='/root/ISOs/CHGA'
+export NSX_T_MANAGER_OVA=$(ls $ROOT_DIR/nsx-mgr-ova)
+export NSX_T_CONTROLLER_OVA=$(ls $ROOT_DIR/nsx-ctrl-ova)
+export NSX_T_EDGE_OVA=$(ls $ROOT_DIR/nsx-edge-ova)
+
+source $FUNCTIONS_DIR/copy_ovas.sh
+source $FUNCTIONS_DIR/create_ansible_cfg.sh
+source $FUNCTIONS_DIR/create_answerfile.sh
+source $FUNCTIONS_DIR/create_hosts.sh
+source $FUNCTIONS_DIR/create_extra_yaml_args.sh
+
+DEBUG=""
+if [ "$ENABLE_ANSIBLE_DEBUG" == "true" ]; then
+  DEBUG="-vvv"
+fi
+
+# Check if NSX MGR is up or not
+nsx_mgr_up_status=$(curl -s -o /dev/null -I -w "%{http_code}" -k  https://${NSX_T_MANAGER_IP}:443/login.jsp || true)
+
+# Deploy the ovas if its not up
+if [ $nsx_mgr_up_status -ne 200 ]; then
+  echo "NSX Mgr not up yet, please deploy the ovas before configuring routers!!" 
+  exit -1
+fi
+
+create_hosts
+create_answerfile
+create_ansible_cfg
+create_extra_yaml_args
+
+cp hosts answerfile.yml ansible.cfg extra_yaml_args.yml nsxt-ansible/.
+cd nsxt-ansible
+
+echo ""
+
+
+
+NO_OF_CONTROLLERS=$(curl -k -u "admin:$NSX_T_MANAGER_ADMIN_PWD" \
+                    https://${NSX_T_MANAGER_IP}/api/v1/cluster/nodes \
+                    | jq '.results[].controller_role.type' | wc -l )
+if [ "$NO_OF_CONTROLLERS" -lt 2 ]; then
+  echo "NSX Mgr and controller not configured yet, please cleanup incomplete vms and rerun base install before configuring routers!!" 
+  exit -1
+fi
+
+ansible-playbook $DEBUG -i hosts configureNsx.yml -e @extra_yaml_args.yml
+STATUS=$?
+
+
+echo ""
+
+
+if [ -z "$SUPPORT_NSX_VMOTION" -o "$SUPPORT_NSX_VMOTION" == "false" ]; then
+  echo "Skipping vmks configuration for NSX-T Mgr!!" 
+else
+  echo "Allowing vmks configuration for NSX-T Mgr!!" 
+  ansible-playbook $DEBUG -i hosts configure_nsx_vmks.yml -e @extra_yaml_args.yml
+
+fi
+
+echo ""
+
