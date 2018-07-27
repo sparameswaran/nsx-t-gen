43b6c89bb1566f2ada2ce32394f54c3ea89faa7c
Sabha Parameswaran <sabhap@pivotal.io>
2018-07-27 15:53:12 -0700

    Move python modules out of config-nsx-t-extras

--- python/client.py
+++ python/client.py
@@ -16,9 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__author__ = 'Sabha Parameswaran'
-
 from __future__ import absolute_import, division, print_function
+
+__author__ = 'Sabha Parameswaran'
 
 import sys
 import yaml
