c4d869582546a9d9366306f32fddfeb93daef351
Sabha Parameswaran <sabhap@pivotal.io>
2018-07-27 16:19:07 -0700

    cleanup wipe-env

--- tasks/uninstall-nsx-t/task.sh
+++ tasks/uninstall-nsx-t/task.sh
@@ -18,13 +18,25 @@ destroy_vms_not_matching_nsx
 
 export ESXI_HOSTS_FILE="$ROOT_DIR/esxi_hosts"
 
+cp $FUNCTIONS_DIR/uninstall-nsx-vibs.yml $ROOT_DIR/
+if [ "$NSX_T_VERSION" == "2.2" ]; then
+  cp $FUNCTIONS_DIR/uninstall-nsx-t-v2.2-vibs.sh $ROOT_DIR/uninstall-nsx-t-vibs.sh
+else # [ "$NSX_T_VERSION" == "2.1" ]; then
+  cp $FUNCTIONS_DIR/uninstall-nsx-t-v2.1-vibs.sh $ROOT_DIR/uninstall-nsx-t-vibs.sh
+fi
+
+cat > $ROOT_DIR/ansible.cfg << EOF
+[defaults]
+host_key_checking = false
+EOF
+
 # Make sure the NSX Mgr is up
 timeout 15 bash -c "(echo > /dev/tcp/${NSX_T_MANAGER_IP}/22) >/dev/null 2>&1"
 status=$?
 if [ "$status" == "0" ]; then
   # Start wiping the NSX Configurations from NSX Mgr,
   # cleaning up the routers, switches, transport nodes and fabric nodes
-  # Also, additionally create the esxi hosts file so we can do vib cleanup (in case things are sticking around)
+  # Additionally create the esxi hosts file so we can do vib cleanup (in case things are sticking around)
   python $PYTHON_LIB_DIR/nsx_t_wipe.py $ESXI_HOSTS_FILE
   STATUS=$?
 
@@ -38,36 +50,38 @@ else
   echo "NSX Manager VM not responding, so cannot delete any related resources within the NSX Management plane"
 fi
 
-echo "Now deleting the NSX vms"
+echo "Going to delete the NSX vms in 30 seconds!!"
+echo "Cancel the task if you want to manually check and then delete the VMs"
+echo "If canncelled, the deletion of the NSX VMs  up as well as removal of vibs from Esxi hosts needs to be done manually!!"
 
+echo "Manual NSX-T Vib removal command:"
+cat $ROOT_DIR/uninstall-nsx-t-vibs.sh
+echo ""
+echo "Related Esxi hosts:"
+cat $ESXI_HOSTS_FILE | grep ansible |  awk '{print $1}' | sed -e 's/://g'
+echo ""
+
+sleep 30
+
+echo "Proceeding with NSX-T Management Plane VM deletion!"
 delete_vm_using_govc "edge"
 delete_vm_using_govc "ctrl"
 delete_vm_using_govc "mgr"
+echo "Finished NSX-T Management Plane VM deletion!"
 
-cp $FUNCTIONS_DIR/uninstall-nsx-vibs.yml $ROOT_DIR/
-if [ "$NSX_T_VERSION" == "2.2" ]; then
-  cp $FUNCTIONS_DIR/uninstall-nsx-t-v2.2-vibs.sh $ROOT_DIR//uninstall-nsx-t-vibs.sh
-else # [ "$NSX_T_VERSION" == "2.1" ]; then
-  cp $FUNCTIONS_DIR/uninstall-nsx-t-v2.1-vibs.sh $ROOT_DIR//uninstall-nsx-t-vibs.sh
-fi
-
-cat > $ROOT_DIR/ansible.cfg << EOF
-[defaults]
-host_key_checking = false
-EOF
-
+sleep 5
 echo "Now removing the NSX-T related vibs from the Esxi Hosts"
 ansible-playbook -i $ESXI_HOSTS_FILE $ROOT_DIR/uninstall-nsx-vibs.yml
 STATUS=$?
 
 echo "NSX-T Vibs removed from the Esxi hosts"
-echo "Related Esxi Hosts:"
 
-# esxi_hosts file gets crated like:
+# esxi_hosts file looks like:
 # esxi_hosts:
 #   hosts:
 #     sc2-host-corp.local.io: { ansible_ssh_host: sc2-host-corp.local.io, ansible_ssh_user: root, ansible_ssh_pass: asdfn3! }
 
+echo "Related Esxi Hosts:"
 echo "--------------------------------------"
 cat $ESXI_HOSTS_FILE | grep ansible |  awk '{print $1}' | sed -e 's/://g'
 echo "--------------------------------------"
@@ -76,6 +90,6 @@ echo ""
 echo "WARNING!! The Esxi hosts should be rebooted for nsx-t vib removal to be effective!"
 echo "Please reboot all the listed Esxi Hosts in a rolling fashion to pick the changes!!"
 echo ""
-echo "NSX-T ${NSX_T_VERSION} Uninstall Complete!!"
+echo "NSX-T ${NSX_T_VERSION} Uninstall Completed!!"
 
 exit $STATUS
