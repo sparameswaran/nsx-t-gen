e73062d0265a7c8119014819e1e9b2161271a72c
Sabha Parameswaran <sabhap@pivotal.io>
2018-07-29 19:21:09 -0700

    Integ changes for wipe refactor

--- functions/deploy_ova_using_govc.sh
+++ functions/deploy_ova_using_govc.sh
@@ -38,14 +38,17 @@ function deploy_ova {
     govc import.ova -options=$ova_options "$path_to_ova"
   else
     set +e
-    found_rp=$(govc find . -name "${resource_pool}" 2>&1 | grep Resources | grep "${resource_pool}$" || true )
+    found_rp=$(govc find . -name "${resource_pool}" -type p 2>&1 | grep Resources | grep "${resource_pool}$" || true )
+    set -e
     if [ "$found_rp" == "" ]; then
       govc pool.create "$GOVC_CLUSTER/Resources/$resource_pool"
       GOVC_RESOURCE_POOL="$GOVC_CLUSTER/Resources/$resource_pool"
     fi
-    set -e
+    GOVC_RESOURCE_POOL=$resource_pool
+
     govc import.ova -pool="$GOVC_RESOURCE_POOL" -options=$ova_options "$path_to_ova"
   fi
+
 }
 
 function deploy_mgr_ova() {
