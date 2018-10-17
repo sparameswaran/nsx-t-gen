d397395c0324519ab4d409cbac9d9cb73d661ad4
Sabha Parameswaran <sparameswaran@gopivotal.com>
2018-10-17 11:40:47 -0700

    Update nsx-t 2.3 vib removal

--- functions/uninstall-nsx-t-v2.3-vibs.sh
+++ functions/uninstall-nsx-t-v2.3-vibs.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-esxcli software vib remove --no-live-install  -n nsxa -n nsx-hyperbus -n nsx-nestdb \
--n nsxcli -n nsx-exporter -n nsx-netcpa  -n nsx-da -n nsx-nestdb-libs -n nsx-rpc-libs \
--n nsx-metrics-libs -n nsx-lldp -n nsx-ctxteng -n nsx-aggservice -n nsx-common-libs \
--n nsx-esx-datapath -n nsx-host -n nsx-support-bundle-client -n nsx-platform-client \
--n nsx-sfhc -n nsx-mpa -n nsx-python-gevent -n nsx-python-greenlet -n nsx-python-protobuf \
--n nsx-shared-libs -n epsec-mux -n nsx-proxy -n nsx-profiling-libs -n nsx-opsagent
+# removed nsxa, nsx-hyperbus, nsx-lldp, nsx-ctxteng in 2.3
+
+esxcli software vib remove --no-live-install  -n nsx-nestdb -n nsxcli \
+-n nsx-exporter -n nsx-netcpa  -n nsx-da -n nsx-nestdb-libs -n nsx-rpc-libs \
+-n nsx-metrics-libs -n nsx-aggservice -n nsx-common-libs -n nsx-esx-datapath \
+-n nsx-host -n nsx-platform-client -n nsx-sfhc -n nsx-mpa -n nsx-python-gevent \
+-n nsx-python-greenlet -n nsx-python-protobuf -n nsx-shared-libs -n nsx-python-logging \
+-n nsx-proxy -n nsx-profiling-libs -n nsx-opsagent -n nsx-cli-libs -n epsec-mux
